3445ffc7179fae5d82c549e8a75277f4be711caf
mgrisole <grisole.maxime@gmail.com>
2018-09-13 18:44:05 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,63 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  div#app
+    header
+      h3
+        | Welcome to my workshop
+      nav
+        router-link.route(v-for="route in routes" :to="route.path")
+          | {{route.name}}
+    router-view
 </template>
+
+<script>
+import Router from '@/router'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      routes: Router.options.routes
+    }
+  },
+  mounted () {
+    console.log(this.routes)
+  }
+}
+</script>
+
 <style lang="stylus">
+@import url('https://fonts.googleapis.com/css?family=Pacifico|Roboto:100');
+
+$blue = #233D4D
+$red = #D05D5B
+
+h3
+  font-family: 'Roboto', sans-serif;
+  font-size 2rem
+  font-weight 100
+  margin-bottom 5vmin
+
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
+  font-family Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+header
+  display flex
+  flex-direction column
+  min-height 20vh
+  display flex
+  align-items center
+  justify-content center
+  border-bottom 0.2vmin solid lighten($red, 20)
+  color $blue
+  padding-bottom 2vmin
+
+  nav
+    display flex
+    .route
+      font-family Pacifico
+      text-decoration none
+      color $blue
+      padding 0 5vmin
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+require('../node_modules/normalize.css/normalize.css')
+
 Vue.config.productionTip = false
 
 new Vue({
